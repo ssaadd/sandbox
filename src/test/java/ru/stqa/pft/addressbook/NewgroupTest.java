713ad367c33b5e15e153fb3fc7efbef265e8f94b
Aleksandr Muravev <muravevkot@gmail.com>
2021-10-30 20:56:02 +0300

    before test login

--- src/test/java/ru/stqa/pft/addressbook/NewgroupTest.java
+++ src/test/java/ru/stqa/pft/addressbook/NewgroupTest.java
@@ -23,16 +23,16 @@ public class NewgroupTest {
     driver = new FirefoxDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testNewGroup() throws Exception {
     driver.get("http://localhost/addressbook/");
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testNewGroup() throws Exception {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();

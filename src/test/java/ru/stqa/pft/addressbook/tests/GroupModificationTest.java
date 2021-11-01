464a4d56b145ddeebf1a12c4e98a31a6e983fb09
Aleksandr Muravev <muravevkot@gmail.com>
2021-11-01 15:07:37 +0300

    test group mod

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase{
     @Test
     public void testGroupMod() throws Exception {
@@ -11,13 +13,13 @@ public class GroupModificationTest extends TestBase{
         if (! app.getGroupHelper().isGroupPresent()){
             app.getGroupHelper().createGroup(new GroupData("test_change", null, null));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup(before -1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() -1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillNewGroup(new GroupData("test_change", "test_change2", "test_change3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
